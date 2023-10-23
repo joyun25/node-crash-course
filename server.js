b6d26d24dea27bcbf2a0aeb08a07a7eed7f43c0a
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-23 10:03:17 +0800

    redirects

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const server = http.createServer((request, response) => {
       path += 'about.html'
       response.statusCode = 200
       break
+    case '/about-me':
+      response.statusCode = 301
+      response.setHeader('Location', '/about')
+      response.end()
+      break
     default:
       path += '404.html'
       response.statusCode = 404
