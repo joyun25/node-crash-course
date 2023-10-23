b72fe5528931124ef09d294a74eba31c60724d5a
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-23 09:33:26 +0800

    response object

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const http = require('http')
 
 const server = http.createServer((request, response) => {
   console.log('request made')
+
+  // ** set header content type
+  // ** text/plain
+  // response.setHeader('Content-Type', 'text/plain')
+  // response.write('hello ninjas')
+  // response.end()
+
+  // ** text/html
+  response.setHeader('Content-Type', 'text/html')
+  response.write('<head><link rel="stylesheet" href=""></head>')
+  response.write('<h1>hello ninjas</h1>')
+  response.end()
 })
 
 server.listen(3000, 'localhost', () => {
