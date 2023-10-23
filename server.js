4f2a4b71c20c5412079c5891219f2fb88d4c4f27
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-23 09:57:03 +0800

    status code

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const server = http.createServer((request, response) => {
   switch(request.url) {
     case '/':
       path += 'index.html'
+      response.statusCode = 200
       break
     case '/about':
       path += 'about.html'
+      response.statusCode = 200
       break
     default:
       path += '404.html'
+      response.statusCode = 404
       break
   }
 
