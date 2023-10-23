2277ff87e211858a5239c1624ed01a0e8d82ce46
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-23 13:23:33 +0800

    add third party middleware: Morgan

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 // express app
 const app = express()
@@ -9,18 +10,8 @@ app.set('view engine', 'ejs')
 // listen for requests
 app.listen(3000)
 
-app.use((req, res, next) => {
-  console.log('new request made:')
-  console.log('host: ', req.hostname)
-  console.log('path: ', req.path)
-  console.log('method: ', req.method)
-  next()
-})
-
-app.use((req, res, next) => {
-  console.log('in the next middleware')
-  next()
-})
+app.use(morgan('dev'))
+// app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   const blogs = [
@@ -41,11 +32,6 @@ app.get('/', (req, res) => {
     title: 'Home',
     blogs
   })
-})
-
-app.use((req, res, next) => {
-  console.log('in the last middleware') // it doesn't show in index page because we already send the response
-  next()
 })
 
 app.get('/about', (req, res) => {
