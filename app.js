40a758437c1cf994969f7a0341bcb8a7123e43fd
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-23 13:19:19 +0800

    introduce middleware and next()

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.set('view engine', 'ejs')
 // listen for requests
 app.listen(3000)
 
+app.use((req, res, next) => {
+  console.log('new request made:')
+  console.log('host: ', req.hostname)
+  console.log('path: ', req.path)
+  console.log('method: ', req.method)
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('in the next middleware')
+  next()
+})
+
 app.get('/', (req, res) => {
   const blogs = [
     {
@@ -28,6 +41,11 @@ app.get('/', (req, res) => {
     title: 'Home',
     blogs
   })
+})
+
+app.use((req, res, next) => {
+  console.log('in the last middleware') // it doesn't show in index page because we already send the response
+  next()
 })
 
 app.get('/about', (req, res) => {
