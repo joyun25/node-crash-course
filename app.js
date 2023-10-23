0e0e6158cde4abb6c0f64a83a7e51718883fe926
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-23 10:28:21 +0800

    routing & html pages

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const app = express()
 app.listen(3000)
 
 app.get('/', (req, res) => {
-  
-  res.send('<h1>home page</h1>')
+  // res.send('<h1>home page</h1>')
+  res.sendFile('./views/index.html', { root: __dirname }) // default is an absolute path
+})
 
+app.get('/about', (req, res) => {
+  // res.send('<h1>about page</h1>')
+  res.sendFile('./views/about.html', { root: __dirname })
 })
